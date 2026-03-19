dcf0cd5d208f451c583e2231b816d4ecb1088df9
son-jun <24_10815@dshs.kr>
2026-03-19 23:20:28 +0900

    fix frontend path

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "caboose.proxy.rlwy.net",
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "••••••••••••••••••••••••••••••••",
